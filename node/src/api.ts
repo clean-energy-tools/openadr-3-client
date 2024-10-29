c9c8b1427e5f70e985754a68a83bb865f97edd07
David Herron <david@davidherron.com>
2024-10-29 17:34:41 +0200

    Stop exporting stuff from components

--- node/src/api.ts
+++ node/src/api.ts
@@ -1,9 +1,9 @@
 
 export * from './client.js';
-export * from './components/events.js';
-export * from './components/programs.js';
-export * from './components/reports.js';
-export * from './components/resources.js';
-export * from './components/subscriptions.js';
-export * from './components/vens.js';
+// export * from './components/events.js';
+// export * from './components/programs.js';
+// export * from './components/reports.js';
+// export * from './components/resources.js';
+// export * from './components/subscriptions.js';
+// export * from './components/vens.js';
 
